e65eab27e4a6da0562cdbe2c6acf8e2f64399a16
Cameron <p2671340@my365.dmu.ac.uk>
2025-03-28 02:41:46 +0000

    Updating code in main.py

--- main.py
+++ main.py
@@ -124,6 +124,8 @@ def logout():
     session.pop('username', None)
     return redirect(url_for('login'))
 
-if __name__ == '__main__':
-    init_db()
-    app.run(host='0.0.0.0', port=8000, debug=True)
+import os
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8000))  # Default to 8000
+    app.run(host="0.0.0.0", port=port, debug=False)
